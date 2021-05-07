65c420c8b52cb5f14f2b2e1441a88b26a559c7ce
shantsis <shainahantsis@Shainas-MacBook-Air.local>
2021-05-06 22:44:41 -0400

    added accessibility and removed access token

--- src/code.ts
+++ src/code.ts
@@ -1,10 +1,8 @@
 figma.showUI(__html__)
-figma.ui.resize(350,425)
+figma.ui.resize(350,360)
 
 figma.ui.onmessage = msg => {
   if (msg.type === 'color') {
-    //console.log(msg)
-    //console.log(msg.data.styles)
     console.log("done running")
   }
 
